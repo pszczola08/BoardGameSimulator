40d39e304e279e0d0bf11a4c5393bbbe6d5bf16d
Wiki wos <162433629+Wikiwos@users.noreply.github.com>
2024-12-07 15:05:40 +0100

    requires approval

--- PlayerCreator.cs
+++ PlayerCreator.cs
@@ -5,6 +5,7 @@ class Player {
     public int Position;
     public string Type;
     public Dictionary<string, double> Abilities;
+    public Dictionary<string, double> SuperAbilities;
     public static List<Player> Players { get; set; } = new();
 
     public Player(string Name) {
@@ -14,22 +15,55 @@ class Player {
         int Rnd = rand.Next(1, 5);
         switch (Rnd) {
             case 1:
+            default:
                 Type = "Warrior";
                 Abilities = new() {
-                    { "Attack", 1.1 }
+                    { "Attack", 4.0 },
+                    { "Hp", 5.0 }
+                };
+                SuperAbilities = new() {
+                    { "MulitiplyAttack", 1.3 }
                 };
                 break;
             case 2:
-                Type = "Mag";
+                Type = "Mage";
+                Abilities = new() {
+                    { "Attack", 2.0 },
+                    { "Hp", 4.0 }
+                };
+                SuperAbilities = new() {
+                    { "Force", 3.0  }
+                };
                 break;
             case 3:
                 Type = "Healer";
                 Abilities = new() {
-                    { "Heal", 5.0 }
+                    { "Attack", 2.0 },
+                    { "Hp", 5.0 }
+                };
+                SuperAbilities = new() {
+                    { "Heal", 4.0 }
                 };
                 break;
-            default:
-                Type = "Player";
+            case 4:
+                Type = "Dwarf";
+                Abilities = new() {
+                    { "Attack", 3.0 },
+                    { "Hp", 5.0 }
+                };
+                SuperAbilities = new() {
+                    { "DoubleAttack", 6.0 }
+                };
+                break;
+            case 5:
+                Type = "Elf";
+                Abilities = new() {
+                    { "Attack", 3.0 },
+                    { "Hp" , 4.0 }
+                };
+                SuperAbilities = new() {
+                    { "Shoot", 3.0 }
+                };
                 break;
         }
         Console.WriteLine($"Player {this.Name} ({Type}) was added!");
